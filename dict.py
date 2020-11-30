d77f37748c731e9bac1906250a95fab3ae325423
Andy McGrath <adwmcgrath@gmail.com>
2020-11-30 13:09:39 +0000

    Delete dict.py

--- dict.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open('german vocab - conjunctions.csv', mode='r') as infile:
-    reader = csv.reader(infile)
-    with open('coors_new.csv', mode='w') as outfile:
-        writer = csv.writer(outfile)
-        mydict = {rows[0]:rows[1] for rows in reader}
-        print (mydict)
